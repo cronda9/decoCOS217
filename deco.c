4ee46852cb12ef219a4116282f3b18cb9b457eb0
cronda9 <70668240+cronda9@users.noreply.github.com>
2020-09-17 23:00:52 -0400

    Add files via upload

---
+++ deco.c
@@ -0,0 +1,140 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+enum State_Type {BASE, COMMENT_START, COMMENT, QUOTE, COMMENT_END, BACKSLASH};
+
+enum State_Type handleBaseState(int c)
+{
+    enum State_Type state;
+    if(c == '/'){
+        state = COMMENT_START;
+    }
+    else if(c == '\'' || c == '\"'){
+        putchar(c);
+        state = QUOTE;
+    }
+    else{
+        putchar(c);
+        state = BASE;
+    }
+    return state;
+}
+
+enum State_Type handleComment_StartState(int c){
+    enum State_Type state;
+    if(c == '*'){
+        state = COMMENT;
+        putchar(' ');
+    }
+    else if(c == '\n'){
+        putchar('/');
+        putchar(c);
+        state = BASE;
+    }
+    else{
+        putchar('/');
+        state = COMMENT_START;
+    }
+    return state;
+}
+
+enum State_Type handleCommentState(int c){
+    enum State_Type state;
+    if(c == '\n'){
+        putchar('\n');
+        state = COMMENT;
+    }
+    else if (c == '*'){
+        state = COMMENT_END;
+    }
+    else{
+        state = COMMENT;
+    }
+    return state;
+}
+
+enum State_Type handleComment_EndState(int c){
+    enum State_Type state;
+    if (c == '/'){
+        state = BASE;
+    }
+    else if (c == '*'){
+        state = COMMENT_END;
+    }
+    else if(c == '\n'){
+        putchar('\n');
+        state = COMMENT;
+    }
+    else{
+        state = COMMENT;
+    }
+    return state;
+}
+
+enum State_Type handleQuoteState(int c){
+    enum State_Type state;
+    if (c == '\'' || c == '\"'){
+        putchar(c);
+        state = BASE;
+    }
+    else if(c == '\\'){
+        putchar(c);
+        state = BACKSLASH;
+    }
+    else{
+        putchar(c);
+        state = QUOTE;
+    }
+    return state;
+}
+
+enum State_Type handleBackSlashState(int c){
+    enum State_Type state;
+    putchar(c);
+    state = QUOTE;
+    return state;
+}
+
+int main(void){
+    int c;
+    enum State_Type state = BASE;
+    int line = 0;
+    int possible_error;
+    while((c = getchar()) != EOF){
+        if (c == '\n')
+            line++;
+        switch (state) {
+            case BASE:
+                state = handleBaseState(c);
+                break;
+            case COMMENT_START:
+                state = handleComment_StartState(c);
+                break;
+            case COMMENT:
+                possible_error = line;
+                state = handleCommentState(c);
+                break;
+            case COMMENT_END:
+                state = handleComment_EndState(c);
+                break;
+            case QUOTE:
+                state = handleQuoteState(c);
+                break;
+            case BACKSLASH:
+                state = handleBackSlashState(c);
+                break;
+            default: break;
+        }
+    }
+
+    if (state == 2){
+        fprintf(stderr, "Error: %d: unterminated comment\n", possible_error);
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
+
+
